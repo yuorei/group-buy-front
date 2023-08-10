584802d2a12bb08d448713482ba78a6be8bbf25d
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-08-10 12:44:39 +0900

    set basePath & assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const repoName = process.env.REPO_NAME || "";
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+}
+
+module.exports = nextConfig;
